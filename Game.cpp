86b96f97a9c13c0c8094c030a2b03af3b3cd1cc2
Surbir Sandhu <surbir.sandhu@yahoo.com>
2020-05-04 11:07:02 -0700

    implements game class

---
+++ Game.cpp
@@ -0,0 +1,101 @@
+#include "Game.h"
+
+void Game::initialize(Player* p1, Player* p2)
+{
+
+       players[0] = p1;
+       players[1] = p2;
+
+        if (p1->isWhiteSide()) {
+            this->currentTurn = p1;
+        }
+        else {
+            this->currentTurn = p2;
+        }
+
+        movesPlayed.clear();
+
+}
+
+bool Game::isEnd()
+{
+    return this->getStatus() != this->ACTIVE ;
+}
+
+bool Game::getStatus()
+{
+    return this->status ;
+}
+
+void Game::setStatus(GameStatus status)
+{
+    this->status = status;
+}
+
+bool Game::playerMove(Player* player, int startX, int startY, int endX, int endY)
+{
+    Spot* startBox = board.boxes[startX][startY];
+    Spot* endBox = board.boxes[startY][endY];
+    Move* move1 = new Move(player, startBox, endBox);
+    return this->makeMove(*move1, player);
+
+}
+
+bool Game::makeMove(Move move1, Player* player)
+{
+Piece* sourcePiece = move1.getStart()->getPiece();
+        if (sourcePiece == nullptr) {
+            return false;
+        }
+
+        // valid player
+        if (player != currentTurn) {
+            return false;
+        }
+
+        if (sourcePiece->isWhite() != player->isWhiteSide()) {
+            return false;
+        }
+
+        // valid move?
+        if (!sourcePiece->canMove(&board, move1.getStart(),
+                                            move1.getEnding())) {
+            return false;
+        }
+
+        // kill?
+        Piece* destPiece = move1.getStart()->getPiece();
+        if (destPiece != nullptr) {
+            destPiece->setKilled(true);
+            move1.setPieceKilled(destPiece);
+        }
+
+
+        // store the move
+        movesPlayed.push_back(move1);
+
+        // move piece from the stat box to end box
+        move1.getEnding()->setPiece(move1.getStart()->getPiece());
+        move1.getStart()->setPiece(nullptr);
+
+        if (destPiece != nullptr && dynamic_cast<King*>(destPiece)) {
+            if (player->isWhiteSide()) {
+                this->setStatus(this->WHITE_WIN);
+            }
+            else {
+                this->setStatus(this->BLACK_WIN);
+            }
+        }
+
+        // set the current turn to the other player
+        if (this->currentTurn == players[0]) {
+            this->currentTurn = players[1];
+        }
+        else {
+            this->currentTurn = players[0];
+        }
+
+        return true;
+    }
+
+
---
+++ Game.h
@@ -0,0 +1,43 @@
+#ifndef GAME_H
+#define GAME_H
+#include "Player.h"
+#include "Board.h"
+#include "Move.h"
+#include "King.h"
+#include <list>
+#include <typeinfo>
+
+class Game
+{
+
+public:
+
+    enum GameStatus
+    {
+        ACTIVE,
+        BLACK_WIN,
+        WHITE_WIN,
+        FORFEIT,
+        STALEMATE,
+        RESIGNATION
+    } ;
+
+     Player* players[2];
+     Board board;
+     Player* currentTurn;
+     GameStatus status;
+     list<Move> movesPlayed;
+
+     void initialize(Player*, Player*) ;
+     bool isEnd() ;
+     bool getStatus() ;
+     void setStatus(GameStatus) ;
+     bool playerMove(Player*, int, int, int, int) ;
+     bool makeMove(Move, Player*) ;
+
+
+
+};
+
+
+#endif // GAME_H
